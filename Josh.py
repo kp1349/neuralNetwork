24d049be1c578110f9ad861d7ad7fabae2123a2b
josh <jsf395@nyu.edu>
2017-04-23 12:32:30 -0400

    asd

--- Josh.py
+++ Josh.py
@@ -8,22 +8,25 @@ class NeuralNetwork:
 
 	def Test(self, data): #josh
 
-	def Forward(self, input_data): #josh
-		if self.input != len(input_data):
-			raise ValueError('unmatched length for inputs')
+	def tanh(x):
+		return (math.exp(x)-math.exp(-x)) / (math.exp(-x)+math.exp(x))
 
-		for i in range(self.input):
+	def sigmoid(x):
+		return 1/(1+(math.exp(-x))
+
+	def Forward(self, input_data): #josh
+		for i in range(0, self.input):
 			self.input_act[i] == inputs[i]
 
-		for i in range(self.hidden):
+		for i in range(0, self.hidden):
 			hidsum = 0.0
-			for j in range(self.input):
+			for j in range(0, self.input):
 				hidsum += self.input_act[j] * self.weights_in[j][i]
 			self.hidden_act[i] = tanh(hidsum)
 
-		for i in range(self.output):
+		for i in range(0, self.output):
 			outsum = 0.0
-			for j in range(self.hidden):
+			for j in range(0, self.hidden):
 				outsum += self.hidden_act[j] * self.weights_out[j][i]
 			self.output_act[i] = sigmoid(outsum)
 

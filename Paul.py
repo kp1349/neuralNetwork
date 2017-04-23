ce689fc24e83f49c195ca0e8a472baa5c8d68e15
Koushik Paul <paul@172-16-29-183.DYNAPOOL.NYU.EDU>
2017-04-23 11:55:15 -0400

    first

--- Paul.py
+++ Paul.py
@@ -1,13 +1,21 @@
+import math
+import random
 
 class NeuralNetwork:
 	def __init__(self, input, hidden, output, iteration, learning_rate, momentum, decay): #paul
+		x=0
 
 	def Train(self, data): #paul
+		x=0
 
 	def Test(self, data): #josh
+		x=0
 
 	def Forward(self, input_data): #josh
+		x=0
 
 	def Backward(self, correct_values): #danny
+		x=0
 
 	def Error(outputs, correct_values): #danny
+		x=0
